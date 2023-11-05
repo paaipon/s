9ac2394614188100410782ec71a662b6ae57bb91
paaipon <148544716+paaipon@users.noreply.github.com>
2023-11-05 19:32:01 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const api = require("@justalk/pornhub-api") 
+const { exec } = require("child_process")
+var m3u8ToMp4 = require("m3u8-to-mp4");
+var converter = new m3u8ToMp4();
+const fs = require("fs")
+var counter = false;
+api.search("chinese teenager girl").then((list) => {
+    list.results.forEach(e => {
+        if(e.premium) return;
+        if(fs.existsSync(`./output/${e.title}.mp4`))return;
+        if(counter) return;
+        counter = true;
+        console.log(e.title)
+        api.page(e.link, ['title','download_urls']).then(async r=>{
+          //  exec(`wget -c -O "./output/${e.title}.m3u8" "${r.download_urls["1080P"]}"`,(a,b,c)=>
+          await converter
+          .setInputFile(r.download_urls["720P"])
+          .setOutputFile("./output/"+e.title+".mp4")
+          .start();
+        });
+    })
+})
