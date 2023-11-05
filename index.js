8ac92ef2db4c97e64d5d05c312998ddb2a38a36a
paaipon <148544716+paaipon@users.noreply.github.com>
2023-11-05 12:20:04 +0000

    fix

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ const api = require("@justalk/pornhub-api")
 const { exec } = require("child_process")
 var m3u8ToMp4 = require("m3u8-to-mp4");
 var converter = new m3u8ToMp4();
+const { PornHub } = require("pornhub.js")
 const fs = require("fs")
 var counter = false;
-api.search("chinese teenager girl").then((list) => {
-    list.results.forEach(e => {
-        if(e.premium) return;
+const hub = new PornHub()
+api.search("chinese teenager girl").then(async (list) => {
+   const results = list.results.filter(r=>r.premium == false)
+      const e = results[Math.floor(Math.random() * results.length)];
         if(fs.existsSync(`./output/${e.title}.mp4`))return;
         if(counter) return;
         counter = true;
         console.log(e.title)
-        api.page(e.link, ['title','download_urls']).then(async r=>{
+        // exec("node r.js download=highest url="+e.link, (a,b,c) => console.log(a,b,c))
           //  exec(`wget -c -O "./output/${e.title}.m3u8" "${r.download_urls["1080P"]}"`,(a,b,c)=>
-          await converter
-          .setInputFile(r.download_urls["720P"])
-          .setOutputFile("./output/"+e.title+".mp4")
-          .start();
-        });
-    })
+        const a = await hub.video(e.link)
+        const d = a.mediaDefinitions.find(e=> e.format == "mp4")
+    if(!d) return;
+    exec(`wget -c -O '${e.title}.mp4' `+d.videoUrl, (a,b,c)=>console.log(a,b,c))
+  
 })
